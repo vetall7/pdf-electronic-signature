673457978e6d8f25dc393308ee6d82e2f977e5c4
Ruslan Rabadanov <ruslanrabadanov2101@gmail.com>
2025-03-09 17:21:00 +0100

    feat: add business functionality, update the UI

--- app/gui.py
+++ app/gui.py
@@ -1,15 +1,20 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog
+import os
+from helper import sign_pdf, verify_pdf
 
 def show_upload_button(mode):
     global selected_mode
     selected_mode = mode
     start_frame.pack_forget()
     upload_frame.pack()
+    cancel_button.pack()
 
 def upload_file():
     file_path.set(filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")]))
     if file_path.get():
+        file_label.config(text=f"Selected file: {os.path.basename(file_path.get())}")
+        file_label.pack()
         upload_frame.pack_forget()
         if selected_mode == "sign":
             pin_frame.pack()
@@ -23,23 +28,37 @@ def enable_go_button(*args):
         go_button.pack_forget()
 
 def process_file():
-    if selected_mode == "sign" and not pin_entry.get():
-        messagebox.showerror("Error", "Please enter a PIN")
+    status = None
+    if selected_mode == "sign":
+        status = sign_pdf(file_path)
+    else:
+        status = verify_pdf(file_path)
+    
+    if not status:
+        cancel_action()
         return
-    # TODO: Implement signing and verification functionality
+
     go_button.pack_forget()
     pin_frame.pack_forget()
     status_label.config(text="Done!")
     status_label.pack()
-    if selected_mode == "sign":
-        download_button.pack()
-
-def download_signed_pdf():
-    # TODO: Implement download functionality
-    messagebox.showinfo("Download", "Signed PDF downloaded successfully!")
     
+def cancel_action():
+    upload_frame.pack_forget()
+    go_button.pack_forget()
+    pin_frame.pack_forget()
+    status_label.pack_forget()
+    cancel_button.pack_forget()
+    file_label.pack_forget()
+    start_frame.pack()
+    file_path.set("")
+    pin_entry.delete(0, tk.END)
+    status_label.config(text="")
+    file_label.config(text="")
+
 root = tk.Tk()
 root.title("PDF Sign & Verify")
+root.geometry("400x150")
 
 file_path = tk.StringVar()
 selected_mode = None
@@ -55,6 +74,8 @@ upload_frame = tk.Frame(root)
 upload_button = tk.Button(upload_frame, text="Upload PDF", command=upload_file)
 upload_button.pack()
 
+file_label = tk.Label(root, text="")
+
 go_button = tk.Button(root, text="Go!", command=process_file)
 
 pin_frame = tk.Frame(root)
@@ -65,6 +86,5 @@ pin_entry.bind("<KeyRelease>", enable_go_button)
 
 status_label = tk.Label(root, text="")
 
-download_button = tk.Button(root, text="Download PDF", command=download_signed_pdf)
-
+cancel_button = tk.Button(root, text="Cancel", command=cancel_action)
 root.mainloop()

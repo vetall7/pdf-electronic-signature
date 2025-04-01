0072c84cc55b889fb44df8a2fbd599bfd8ce6076
vetall7 <shapovalovvit228@gmail.com>
2025-04-01 15:08:40 +0200

    doc: add documentation for the second app

--- rsa-keys-generator/main.py
+++ rsa-keys-generator/main.py
@@ -1,3 +1,8 @@
+"""
+@file: main.py
+@brief: This script generates RSA keys and encrypts the private key using a PIN.
+@details: The script uses the cryptography library to generate RSA keys and encrypt the private key.
+"""
 import getpass
 import hashlib
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -5,6 +10,11 @@ from cryptography.hazmat.primitives import serialization
 
 
 def generate_rsa_keys():
+    """
+    @brief: This function generates RSA keys.
+    @details: It generates a private key and derives the public key from it.
+    @return: The generated private and public keys.
+    """
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=4096
@@ -16,6 +26,12 @@ def generate_rsa_keys():
 
 
 def transform_pin_to_key_256(pin):
+    """
+    @brief: This function transforms a PIN into a 256-bit key using SHA-256 hashing.
+    @details: It encodes the PIN and hashes it to create a key.
+    @param pin: The PIN to be transformed into a key.
+    @return: The generated key.
+    """
     print("🔐 Transforming pin to key...")
     
     hash = hashlib.sha256(pin.encode()).digest()
@@ -26,7 +42,13 @@ def transform_pin_to_key_256(pin):
 
 
 def encrypt_private_key(private_key, pin):
-
+    """
+    @brief: This function encrypts the private key using a PIN.
+    @details: It uses the PIN to derive a key and encrypts the private key.
+    @param private_key: The private key to be encrypted.
+    @param pin: The PIN used to encrypt the private key.
+    @return: The encrypted private key.
+    """
     key = transform_pin_to_key_256(pin)
 
     private_key_bytes = private_key.private_bytes(
@@ -39,12 +61,22 @@ def encrypt_private_key(private_key, pin):
 
 
 def save_private_key(encrypted_private_key):
+    """"
+    @brief: This function saves the encrypted private key to a file.
+    @details: It writes the encrypted private key to a file named 'private_key.pem'.
+    @param encrypted_private_key: The encrypted private key to be saved.
+    """
     with open("private_key.pem", "wb") as f:
         f.write(encrypted_private_key)
         print("🔑 Private key saved successfully to private_key.pem")
 
 
 def save_public_key(public_key):
+    """"
+    @brief: This function saves the public key to a file.
+    @details: It writes the public key to a file named 'public_key.pem'.
+    @param public_key: The public key to be saved.
+    """
     with open("public_key.pem", "wb") as f:
         f.write(public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
